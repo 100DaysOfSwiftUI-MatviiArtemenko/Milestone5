923d40a003bcb4bb904b4df5d911b440e760ab8d
Matvii Artemenko <100190098+MatveyArtemenko@users.noreply.github.com>
2022-09-20 19:43:26 +0200

    Initial Commit

---
+++ Milestone5/Milestone5App.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone5App.swift
+//  Milestone5
+//
+//  Created by admin on 20.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Milestone5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
